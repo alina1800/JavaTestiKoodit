5014e9c2703705dbef1c5afba3a771b7f2b764a0
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2021-02-15 15:28:09 +0200

    Lisätty run-metodi

--- Testi/src/Data/DataTestit.java
+++ Testi/src/Data/DataTestit.java
@@ -1,5 +1,20 @@
 package Data;
 
-public class DataTestit {
+public class DataTestit implements Runnable{
+	
+	protected String nimi;
+	
+	public DataTestit(String nimi)
+	{
+		this.nimi = nimi;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		System.out.print(nimi + " käynnistyi");
+		
+	}
 
 }
