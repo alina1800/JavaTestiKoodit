8b31ef3284b6d3b7a953e84b92d003abf91bc0b2
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2021-02-18 11:22:03 +0200

    Ohjelma käynnissä poistettu

--- EkaLegoTesti/src/App/LegoAppTesti.java
+++ EkaLegoTesti/src/App/LegoAppTesti.java
@@ -8,7 +8,7 @@ public class LegoAppTesti {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Ohjelma käynnissä");
+		
 		System.out.println("Moi Jenni!");
 		System.out.println("Moi Liisa!");
 		System.out.println("Moi Alina!");
