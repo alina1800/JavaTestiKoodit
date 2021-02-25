5309c8f2ee1ae3155beec6d57e7ad2dc634361a0
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2021-02-25 09:35:43 +0200

    Rename Distance

--- EkaLegoTesti/src/Data/Distance.java
+++ EkaLegoTesti/src/Data/Distance.java
@@ -2,11 +2,14 @@ package Data;
 
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.utility.Delay;
 import lejos.hardware.motor.*;
 
 public class Distance implements Runnable{
 	
 	float                range;
+	
     UltraSonicSensor     uss = new UltraSonicSensor(SensorPort.S4);
     
     range = uss.getRange();
